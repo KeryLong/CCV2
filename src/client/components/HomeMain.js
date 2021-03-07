dc286d11718ec6752b441735ab7adc83be2f077a
kerylong <kery.long@gmail.com>
2021-03-06 21:43:59 -0700

    getting started with material UI

---
+++ src/client/components/HomeMain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheets/App.css'
+import Container from '@material-ui/core/Container';
+
+const HomeMain = () =>{
+    return(
+    <Container id="HomeMainContainer">
+        
+    </Container>
+    )
+
+};
+export default HomeMain
