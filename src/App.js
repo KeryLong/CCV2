0da12513587610b183288a740d07994a29245556
kerylong <kery.long@gmail.com>
2021-03-06 21:48:06 -0700

    material ui integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/Logo.png';
+import './client/stylesheets/App.css';
+import 'fontsource-roboto';
+import AppBar from './client/components/appBar';
+import LogoGrid from './client/components/logoBanner';
+import HomeMain from './client/components/HomeMain'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar />
+      <LogoGrid />
+      <HomeMain />
+    <img src="/client/Images/Main.png" />
     </div>
   );
+  
 }
-
 export default App;
+
